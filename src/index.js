a896aeaeab123038d11cde5d08b9b27917749147
Cris <krontroth@gmail.com>
2024-03-18 21:12:52 -0500

    crear endpoints

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 //archivo desde donde arranca el servidor
 const express = require('express');
-const morgan = require('morgan')
-
+const morgan = require('morgan');
+const taskRoutes = require('./routes/tasks.routes')
 const app = express();
 
 app.use(morgan('dev'))
-
+app.use(taskRoutes);
 app.listen(4000)
-
 console. log('Server on port 4000')
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,1 +1,27 @@
 //se definen las rutas de los endpoints
+const {Router} = require('express');
+
+const router = Router();
+
+//router.get('/', (reg, res) => {
+    //res.send('Hello world');
+  //  }
+//)
+
+router.get('/tasks', (req, res) => {
+    res.send('retrieving a list of tasks');
+})
+router.get('/tasks/10', (req, res) => {
+    res.send('retrieving a single task');
+})
+router.post('/tasks', (req, res) => {
+    res.send('creating a task');
+})
+router.delete('/tasks', (req, res) => {
+    res.send('deleting a task');
+})
+router.put('/tasks', (reg, res) => {
+    res.send('updating a task');
+})
+
+module.exports = router;
